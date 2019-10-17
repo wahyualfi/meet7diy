14095615ccbb5bd3e13b6a43c0a19854c41d0674
agit-amrullah <agitamrullah@qatros.com>
2019-10-17 20:16:14 +0700

    Meet 9

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -4,20 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Barang;
+use Validator;
+
+/*
+* CONTOH PENGGUNAAN HELPER VALIDATOR
+*/
 
 class BarangController extends Controller
 {
     public function simpanBarang(Request $r)
     {
-    	// $r->nama_barang
-		// $r->harga_barang
-		// $r->qty_barang
-		$data 	= new Barang();
-		$data->nama_barang 	= $r->nama_barang;
-		$data->harga_barang = $r->harga_barang;
-		$data->qty 			= $r->qty_barang;
-		$data->save();
+    	// Validasi requiest
+    	$validator = Validator::make($r->all(),[
+    		'nama_barang' => 'required|min:10'
+    	]);
 
-		return redirect('/input-barang');
+    	// Pengecekan apabila terjadi kesalahan validasi
+    	if($validator->fails()){
+    		abort(403, "Kesalahan input.");
+    	} else {
+    	// Aksi apabila tidak ada kesalahan validasi
+    		try{
+    			// Mencoba eksekusi perintah
+				$data 	= new Barang();
+				$data->nama_barang 	= $r->nama_barang;
+				$data->harga_barang = $r->harga_barang;
+				$data->qty 			= $r->qty_barang;
+				$data->save();
+
+				return redirect('/input-barang');
+    		} catch(Exception $e){
+    			// Redirect apabila terjadi kesalahan
+    			return view('errors.123');
+    		}
+    	}
     }
 }
